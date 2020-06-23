261ddf6dcf50ecbfa213b5d43a8d4e91740528f6
SUMIT <64258056+malisumit86@users.noreply.github.com>
2020-06-23 13:32:18 +0530

    first commit

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 setInterval(function() {
     var date = new Date();
-
-    updateClock(date);
     updateTime(date);
+    updateClock(date);
+
+    getday(date);
+
 }, 1000);
 
 function updateTime(date) {
@@ -19,4 +21,25 @@ function updateClock(date) {
     secHand.transform = "rotate(" + date.getSeconds() * 6 + "deg)";
     min.transform = "rotate(" + date.getMinutes() * 6 + "deg)";
     hrs.transform = "rotate(" + (date.getHours() * 30 + date.getMinutes() * 0.5) + "deg)";
+}
+
+function getday(date) {
+    var d = document.getElementById("day");
+    var tarikh = document.getElementById("date");
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    var n = weekday[date.getDay()];
+    d.innerHTML = n;
+    var mon = date.getMonth() + 1;
+
+    tarikh.innerHTML = date.getDate() + "/" + mon + "/" + date.getFullYear();
+
+
 }
